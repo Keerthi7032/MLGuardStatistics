a5c36f0ff1ed7da60b05b3c04e9e8ea67dac19b8
ManojKandala <manojkandala@gmail.com>
2018-04-15 23:04:46 +0530

    Added uptime and downtime

--- polls/reports.py
+++ polls/reports.py
@@ -1,12 +1,6 @@
 import pandas as pd
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfTransformer
-from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.pipeline import Pipeline
-from IPython.display import display, HTML
 import MySQLdb
-import datetime as dt
+from datetime import datetime
 from collections import OrderedDict
 
 
@@ -40,28 +34,76 @@ def get_statistics(df,lis,uni_cid,grouped):
     return dic2
 
 def get():
-	div=[]
-	conn = MySQLdb.connect(host="107.180.71.58",
-                  port=3306,
-                  user="root",
-                  passwd="root",
-                  db="mlcharts")
-	cur=conn.cursor()
-	df=pd.read_sql("Select in_time,cid from faces_log",conn)
-	df['year'] = df['in_time'].dt.year
-	df['month'] = df['in_time'].dt.month
-	df['day'] = df['in_time'].dt.day
-	df['time'] = df['in_time'].dt.hour
-	df['date'] = df['in_time'].dt.date
-	df.drop('in_time',axis=1,inplace=True)
-	uni_cid = list(df['cid'].unique())
-	l=['year','month','day','time','date']
-	df1=get_for('year',df,uni_cid)
-	df2=get_for('month',df,uni_cid)
-	df3=get_for('day',df,uni_cid)
-	df4=get_for('date',df,uni_cid)
-	div.append(df1.to_html())
-	div.append(df2.to_html())
-	div.append(df3.to_html())
-	div.append(df4.to_html())
-	return div
+    div=[]
+    conn = MySQLdb.connect(host="107.180.71.58", port=3306, user="root", passwd="root", db="mlcharts")
+    df=pd.read_sql("Select in_time,cid from faces_log",conn)
+    df['year'] = df['in_time'].dt.year
+    df['month'] = df['in_time'].dt.month
+    df['day'] = df['in_time'].dt.day
+    df['time'] = df['in_time'].dt.hour
+    df['date'] = df['in_time'].dt.date
+    df.drop('in_time',axis=1,inplace=True)
+    uni_cid = list(df['cid'].unique())
+    l=['year','month','day','time','date']
+    df1=get_for('year',df,uni_cid)
+    df2=get_for('month',df,uni_cid)
+    df3=get_for('day',df,uni_cid)
+    df4=get_for('date',df,uni_cid)
+    div.append(df1.to_html())
+    div.append(df2.to_html())
+    div.append(df3.to_html())
+    div.append(df4.to_html())
+
+    # get uptime of all MLGuards
+    query = "SELECT cid, last_uptime FROM uptime"
+    uptime_df = pd.read_sql(query, conn)
+    div.append(uptime_df.to_html())
+
+    # get downtime of all MLGuards
+    div.append(check_status())
+    return div
+
+
+def check_status():
+    uptimes_all = {}
+    company = {}
+    status_df = pd.DataFrame()
+    cur_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    cur_time = datetime.strptime(cur_time, '%Y-%m-%d %H:%M:%S')
+
+    query = "SELECT cid, name FROM company"
+    conn = MySQLdb.connect(host="107.180.71.58", port=3306, user="root", passwd="root", db="mlcharts")
+    cur = conn.cursor()
+    cur.execute(query)
+    rows = cur.fetchall()
+    for i in range(len(rows)):
+        company[rows[i][0]] = rows[i][1]
+
+    query = "SELECT cid, last_uptime FROM uptime"
+    conn = MySQLdb.connect(host="107.180.71.58", port=3306, user="root", passwd="root", db="mlcharts")
+    cur = conn.cursor()
+    cur.execute(query)
+    rows = cur.fetchall()
+
+    for i in range(len(rows)):
+        uptimes_all[rows[i][0]] = rows[i][1]
+
+    for cid, uptime in uptimes_all.items():
+
+        l=[]
+        if uptime == None:
+            uptime = datetime.strptime('2018-03-15 00:00:00', '%Y-%m-%d %H:%M:%S')
+        diff_time = cur_time - uptime
+
+        if (diff_time.seconds > 3900):
+            l.append(company[cid])
+            l.append("The MLGuard is down !!")
+            l.append(diff_time)
+            df2 = pd.DataFrame([l],columns=['Device Name', 'Status', 'Down Time Duration'])
+        else:
+            l.append(company[cid])
+            l.append("The MLGuard is up !!")
+            l.append("00:00:00")
+            df2 = pd.DataFrame([l],columns=['Device Name', 'Status', 'Down Time Duration'])
+        status_df=status_df.append(df2, ignore_index=True)
+    return status_df.to_html()
--- polls/views.py
+++ polls/views.py
@@ -5,5 +5,5 @@ from polls import reports
 def review(request):
     m="hello"
     h=reports.get()
-    return render_to_response('Statistics.html',{"year":h[0],"month":h[1],"day":h[2],"date":h[3]})
+    return render_to_response('Statistics.html',{"year":h[0],"month":h[1],"day":h[2],"date":h[3], "uptime":h[4], "downtime":h[5]})
     #return render_to_response("Welcome.html",{'message':m})
