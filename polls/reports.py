5cb49a645753ffc045f07fc7efa72dca30fed089
unknown <keerthikk7032@gmail.com>
2018-04-19 19:01:52 +0530

    changed

--- polls/reports.py
+++ polls/reports.py
@@ -1,18 +1,24 @@
 import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.pipeline import Pipeline
+from IPython.display import display, HTML
 import MySQLdb
-from datetime import datetime
+import pandas as pd
+import datetime as dt
 from collections import OrderedDict
 
 
 def get_for(st,df,uni_cid):
-    lis=[st,'Office','Banglore','Keerthi','Goutham','Device8','Device9','Manoj']
+    lis=[st,'Office','Banglore','Keerthi','Ravi','Alekhya','Goutham','Device8','Device9','Siri','Manoj']
     grouped=df.groupby([st,'cid'])
     year_statis = get_statistics(df,lis,uni_cid,grouped)
     df_year=pd.DataFrame(columns=lis)
     ldf=[]
     j=0
-    for i in year_statis.keys():
-        
+    for i in year_statis.keys():       
         ldf=[i]+list(OrderedDict(sorted(year_statis[i].items())).values())
         df_year.loc[j] = ldf
         j=j+1
@@ -27,7 +33,7 @@ def get_statistics(df,lis,uni_cid,grouped):
         #print(group['cid'].count())
         if(name[0] in dic2.keys()):
             dic2[name[0]][name[1]]=group['cid'].count()
-        else:
+        else:   
             dic2[name[0]]=dict(zip(uni_cid,[0]*len(uni_cid)))
             dic2[name[0]][name[1]]=group['cid'].count()
         #print(dic2)
@@ -35,7 +41,12 @@ def get_statistics(df,lis,uni_cid,grouped):
 
 def get():
     div=[]
-    conn = MySQLdb.connect(host="107.180.71.58", port=3306, user="root", passwd="root", db="mlcharts")
+    conn = MySQLdb.connect(host="107.180.71.58",
+                  port=3306,
+                  user="root",
+                  passwd="root",
+                  db="mlcharts")
+    cur=conn.cursor()
     df=pd.read_sql("Select in_time,cid from faces_log",conn)
     df['year'] = df['in_time'].dt.year
     df['month'] = df['in_time'].dt.month
@@ -53,57 +64,4 @@ def get():
     div.append(df2.to_html())
     div.append(df3.to_html())
     div.append(df4.to_html())
-
-    # get uptime of all MLGuards
-    query = "SELECT cid, last_uptime FROM uptime"
-    uptime_df = pd.read_sql(query, conn)
-    div.append(uptime_df.to_html())
-
-    # get downtime of all MLGuards
-    div.append(check_status())
     return div
-
-
-def check_status():
-    uptimes_all = {}
-    company = {}
-    status_df = pd.DataFrame()
-    cur_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    cur_time = datetime.strptime(cur_time, '%Y-%m-%d %H:%M:%S')
-
-    query = "SELECT cid, name FROM company"
-    conn = MySQLdb.connect(host="107.180.71.58", port=3306, user="root", passwd="root", db="mlcharts")
-    cur = conn.cursor()
-    cur.execute(query)
-    rows = cur.fetchall()
-    for i in range(len(rows)):
-        company[rows[i][0]] = rows[i][1]
-
-    query = "SELECT cid, last_uptime FROM uptime"
-    conn = MySQLdb.connect(host="107.180.71.58", port=3306, user="root", passwd="root", db="mlcharts")
-    cur = conn.cursor()
-    cur.execute(query)
-    rows = cur.fetchall()
-
-    for i in range(len(rows)):
-        uptimes_all[rows[i][0]] = rows[i][1]
-
-    for cid, uptime in uptimes_all.items():
-
-        l=[]
-        if uptime == None:
-            uptime = datetime.strptime('2018-03-15 00:00:00', '%Y-%m-%d %H:%M:%S')
-        diff_time = cur_time - uptime
-
-        if (diff_time.seconds > 3900):
-            l.append(company[cid])
-            l.append("The MLGuard is down !!")
-            l.append(diff_time)
-            df2 = pd.DataFrame([l],columns=['Device Name', 'Status', 'Down Time Duration'])
-        else:
-            l.append(company[cid])
-            l.append("The MLGuard is up !!")
-            l.append("00:00:00")
-            df2 = pd.DataFrame([l],columns=['Device Name', 'Status', 'Down Time Duration'])
-        status_df=status_df.append(df2, ignore_index=True)
-    return status_df.to_html()
